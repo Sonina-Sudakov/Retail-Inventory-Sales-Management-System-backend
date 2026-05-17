3d259c9a475b05a5670a3c91b6b5d441830f31a2
egogoboy <egor2005sudakov@gmail.com>
2026-05-17 13:47:12 +0300

    fix(models): fix typo in shop.py, remove unique=True from shop.py

--- backend/app/db/models/shop.py
+++ backend/app/db/models/shop.py
@@ -15,31 +15,26 @@ class Shop(Base):
 
     name: Mapped[str] = mapped_column(
         String(60),
-        unique=True,
         nullable=False
     )
 
-    adress: Mapped[str] = mapped_column(
+    address: Mapped[str] = mapped_column(
         String(100),
-        unique=True,
         nullable=False
     )
 
     contact_face: Mapped[str] = mapped_column(
         String(100),
-        unique=True,
         nullable=False
     )
 
     phone_number: Mapped[str] = mapped_column(
         String(16),
-        unique=True,
         nullable=False
     )
 
     email: Mapped[str] = mapped_column(
         String(60),
-        unique=True,
         nullable=False
     )
 
