3ef23a6e58c91809c93cfbcc93c55e9af02c895a
egogoboy <egor2005sudakov@gmail.com>
2026-05-17 10:23:49 +0300

    feat(models): implement user model and __init__.py

---
+++ backend/app/db/models/user.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+from sqlalchemy import String, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True
+    )
+
+    username: Mapped[str] = mapped_column(
+        String(20),
+        unique=True,
+        nullable=False
+    )
+
+    fullname: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False
+    )
+
+    hash_password: Mapped[str] = mapped_column(
+        String(60),
+        nullable=False
+    )
+
+    role: Mapped[str] = mapped_column(
+        String(15),
+        nullable=False
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        nullable=False
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        default=func.now(), 
+        onupdate=func.now(),
+        nullable=False
+    )
