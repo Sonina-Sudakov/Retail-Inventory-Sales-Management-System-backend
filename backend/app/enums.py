457e99181961e817691aba7b179792d2b02a9ae7
sonina-marina <mar1na.sonina@yandex.ru>
2026-05-17 17:14:29 +0300

    feat(enum): add enum classes ShipmentFromLocation and ShipmentStatus

--- backend/app/enums.py
+++ backend/app/enums.py
@@ -6,7 +6,20 @@ class UserRole(str, Enum):
     STOREKEEPER = 'STOREKEEPER'
     SHOPKEEPER = 'SHOPKEEPER'
 
+
 class OrderStatus(str, Enum):
     PENDING = 'PENDING'
     ACCEPTED = 'ACCEPTED'
     CANCELED = 'CANCELED'
+
+
+class ShipmentStatus(str, Enum):
+    CREATED = 'CREATED'
+    IN_DELIVERY = "IN_DELIVERY"
+    CANCELED = 'CANCELED'
+    ACCEPTED = 'ACCEPTED'
+
+
+class ShipmentFromLocation(str, Enum):
+    SUPPLIER = 'SUPPLIER'
+    WAREHOUSE = 'WAREHOUSE'
