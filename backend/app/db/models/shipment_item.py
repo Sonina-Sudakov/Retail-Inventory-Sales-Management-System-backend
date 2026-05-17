df6312081492d66d70337eeb130fc33499281f2a
egogoboy <egor2005sudakov@gmail.com>
2026-05-17 21:29:37 +0300

    feat(models): add CheckConstraint(quantity >= 0) to quantity field and remove extra imports in shimpent_item.py

--- backend/app/db/models/shipment_item.py
+++ backend/app/db/models/shipment_item.py
@@ -1,9 +1,6 @@
-from decimal import Decimal
-
-from sqlalchemy import Numeric, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-
 from base import Base
+from sqlalchemy import CheckConstraint, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 class ShipmentItem(Base):
@@ -28,5 +25,6 @@ class ShipmentItem(Base):
     )
 
     quantity: Mapped[int] = mapped_column(
+        CheckConstraint('quantity >= 0'),
         nullable=False
     )
