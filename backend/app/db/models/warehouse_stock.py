a3be697876fd22b3921e7307e270716d3a9f0fea
egogoboy <egor2005sudakov@gmail.com>
2026-05-17 21:35:49 +0300

    feat(models): add CheckConstraint(quantity >= 0) in quantity field and rearrange imports in warehouse_stock.py

--- backend/app/db/models/warehouse_stock.py
+++ backend/app/db/models/warehouse_stock.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 
-from sqlalchemy import ForeignKey, String, func
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-
 from base import Base
+from sqlalchemy import CheckConstraint, ForeignKey, String, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 class WarehouseStock(Base):
@@ -28,6 +27,7 @@ class WarehouseStock(Base):
     )
 
     quantity: Mapped[int] = mapped_column(
+        CheckConstraint('quantity >= 0'),
         nullable=False
     )
 
