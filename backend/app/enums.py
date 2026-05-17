6f7b18f938013cb0b7e28c0a99882dc91787a79f
egogoboy <egor2005sudakov@gmail.com>
2026-05-17 13:45:25 +0300

    feat(enums): add UserRole enum class

--- backend/app/enums.py
+++ backend/app/enums.py
@@ -1,6 +1,11 @@
 from enum import Enum
 
 
+class UserRole(str, Enum):
+    ADMIN = 'ADMIN'
+    STOREKEEPER = 'STOREKEEPER'
+    SHOPKEEPER = 'SHOPKEEPER'
+
 class OrderStatus(str, Enum):
     PENDING = 'PENDING'
     ACCEPTED = 'ACCEPTED'
