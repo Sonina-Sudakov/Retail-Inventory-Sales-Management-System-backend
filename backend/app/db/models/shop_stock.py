e8cdd2dd3fd3719a8a891f7031687fff4d15e140
egogoboy <egor2005sudakov@gmail.com>
2026-05-17 21:34:12 +0300

    feat(models): add CheckConstraint(quantity >= 0) in quanity and min_quantity fields and remove extra imports in shop_stock.py

--- backend/app/db/models/shop_stock.py
+++ backend/app/db/models/shop_stock.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 
-from sqlalchemy import String, func, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-
 from base import Base
+from sqlalchemy import CheckConstraint, ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 class ShopStock(Base):
@@ -28,10 +27,12 @@ class ShopStock(Base):
     )
 
     quantity: Mapped[int] = mapped_column(
+        CheckConstraint('quantity >= 0'),
         nullable=False
     )
 
     min_quantity: Mapped[int] = mapped_column(
+        CheckConstraint('min_quantity >= 0'),
         nullable=False
     )
 
