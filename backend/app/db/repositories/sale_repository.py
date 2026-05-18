b67eb1136af049ed011239c2059d99699a0451e6
sonina-marina <mar1na.sonina@yandex.ru>
2026-05-18 20:27:52 +0300

    feat(repositories): implement sale_repository.py

---
+++ backend/app/db/repositories/sale_repository.py
@@ -0,0 +1,26 @@
+from sqlalchemy.sql.expression import select
+from app.db.models.sale import Sale
+from app.db.repositories.base_repository import BaseRepository
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class SaleRepository(
+    BaseRepository[Sale]
+):
+    def __init__(
+        self,
+        session: AsyncSession
+    ):
+        super().__init__(Sale, session)
+
+
+    async def get_shop_sales(
+        self,
+        shop_id: int
+    ) -> list[Sale]:
+        
+        stmt = select(Sale).where(Sale.id == shop_id)
+
+        result = await self.session.execute(stmt)
+
+        return list(result.scalars().all())
