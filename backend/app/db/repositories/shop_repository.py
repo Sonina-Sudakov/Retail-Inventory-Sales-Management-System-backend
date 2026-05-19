66e6e574a74fdb3c56aead41aff19a179771f9a6
sonina-marina <mar1na.sonina@yandex.ru>
2026-05-19 07:29:30 +0300

    feat(repositories): implement shop_repository.py

---
+++ backend/app/db/repositories/shop_repository.py
@@ -0,0 +1,13 @@
+from app.db.models.shop import Shop
+from app.db.repositories.base_repository import BaseRepository
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+
+class ShopRepository(
+    BaseRepository[Shop]
+):
+    def __init__(
+        self,
+        session: AsyncSession
+    ):
+        super().__init__(Shop, session)
