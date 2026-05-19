00a0475d04aa2fa63b9f85a5f12a5ea7de9d76a2
sonina-marina <mar1na.sonina@yandex.ru>
2026-05-19 08:17:39 +0300

    feat(repositories): implement shipment_repository.py

---
+++ backend/app/db/repositories/shipment_repository.py
@@ -0,0 +1,40 @@
+from sqlalchemy.sql.expression import select
+from app.db.models.shipment import Shipment
+from app.db.repositories.base_repository import BaseRepository
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from enums import ShipmentStatus
+
+
+class ShipmentRepository(
+    BaseRepository[Shipment]
+):
+    def __init__(
+        self,
+        session: AsyncSession
+    ):
+        super().__init__(Shipment, session)
+
+
+    async def get_shop_shipments(
+        self,
+        shop_id: int
+    ) -> list[Shipment]:
+        
+        stmt = select(Shipment).where(Shipment.to_shop_id == shop_id)
+
+        result = await self.session.execute(stmt)
+
+        return list(result.scalars().all())
+
+   
+    async def get_shipments_by_status(
+        self,
+        status: ShipmentStatus
+    ) -> list[Shipment]:
+
+        stmt = select(Shipment).where(Shipment.status == status)
+
+        result = await self.session.execute(stmt)
+
+        return list(result.scalars().all())        
