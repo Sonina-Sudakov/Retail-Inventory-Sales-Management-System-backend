26829ff3590509deb631d47a1b62c3e84f59a499
egogoboy <egor2005sudakov@gmail.com>
2026-05-19 17:54:07 +0300

    feat(repositories): implement warehouse_stock_repository.py

---
+++ backend/app/db/repositories/warehouse_stock_repository.py
@@ -0,0 +1,52 @@
+from app.db.models.product import Product
+from app.db.models.warehouse_stock import WarehouseStock
+from app.db.repositories.base_repository import BaseRepository
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from sqlalchemy.sql.expression import select
+
+
+class WarehouseStockRepository(
+    BaseRepository[WarehouseStock]
+):
+    def __init__(
+        self,
+        session: AsyncSession
+    ):
+        super().__init__(WarehouseStock, session)
+
+
+    async def get_product_in_warehouse(
+        self,
+        product_id: int
+    ) -> list[WarehouseStock]:
+        
+        stmt = (
+            select(
+                WarehouseStock,
+                Product.name,
+                Product.unit
+            )
+            .join(Product)
+            .where(WarehouseStock.product == product_id))
+
+        result = await self.session.execute(stmt)
+
+        return list(result.scalars().all())
+
+
+    async def get_warehouse_stocks(
+        self
+    ) -> list[WarehouseStock]:
+        
+        stmt = (
+            select(
+                WarehouseStock,
+                Product.name,
+                Product.unit
+            )
+            .join(Product)
+        )
+
+        result = await self.session.execute(stmt)
+
+        return list(result.scalars().all())
