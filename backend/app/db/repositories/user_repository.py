2625911a0d436d8111c2093232c8e4c65372a197
egogoboy <egor2005sudakov@gmail.com>
2026-05-25 10:01:12 +0300

    feat(repositories): add get_by_username() in user repository

--- backend/app/db/repositories/user_repository.py
+++ backend/app/db/repositories/user_repository.py
@@ -1,7 +1,6 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from app.db.models.user import User
 from app.db.repositories.base_repository import BaseRepository
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class UserRepository(
@@ -12,6 +11,17 @@ class UserRepository(
         session: AsyncSession
     ):
         super().__init__(User, session)
+
+
+    async def get_by_username(
+        self,
+        username: str
+    ) -> User | None:
+
+        return await self.session.get(
+            self.model,
+            username
+        )
 
     
     async def update_password(
