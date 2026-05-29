007f6a1ae9ecc6164691e3928be276e0a7fbe091
egogoboy <egor2005sudakov@gmail.com>
2026-05-29 20:08:12 +0300

    fix(api/dependencies): implement get_user_service and get_user_repository correctly

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -1,18 +1,19 @@
+from app.db.dependencies import get_session
+from app.db.repositories.user import UserRepository
+from app.services.user import UserService
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.db.repositories.user_repository import UserRepository
-from app.db.session import get_session
-from app.services.user import UserService
+
+async def get_user_repository(
+    session: AsyncSession = Depends(get_session)
+) -> UserRepository:
+
+    return UserRepository(session)
 
 
 async def get_user_service(
-    session: AsyncSession = Depends(get_session)
+    repository: UserRepository = Depends(get_user_repository)
 ) -> UserService:
 
-    user_repository = UserRepository(session)
-
-    return UserService(
-        session=session,
-        user_repository=user_repository
-    )
+    return UserService(repository)
