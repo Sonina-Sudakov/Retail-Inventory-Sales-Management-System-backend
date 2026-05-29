579ae7e123365931061122138955239b2a914025
egogoboy <egor2005sudakov@gmail.com>
2026-05-29 16:13:21 +0300

    fix(repositories): remove update_password and update_fullname methods from user repository

--- backend/app/db/repositories/user_repository.py
+++ backend/app/db/repositories/user_repository.py
@@ -1,6 +1,7 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.db.models.user import User
 from app.db.repositories.base_repository import BaseRepository
-from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class UserRepository(
@@ -22,37 +23,3 @@ class UserRepository(
             self.model,
             username
         )
-
-    
-    async def update_password(
-        self,
-        user: User,
-        new_hashed_password: str
-    ) -> User:
-
-        user.hash_password = new_hashed_password
-
-        self.session.add(user)
-
-        await self.session.flush()
-
-        await self.session.refresh(user)
-
-        return user
-
-
-    async def update_fullname(
-        self,
-        user: User,
-        new_fullname: str
-    ) -> User:
-
-        user.fullname = new_fullname
-
-        self.session.add(user)
-
-        await self.session.flush()
-
-        await self.session.refresh(user)
-
-        return user
