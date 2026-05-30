f612add5217e26691bcf0e38309a15cb3e0bbab0
egogoboy <egor2005sudakov@gmail.com>
2026-05-30 21:04:11 +0300

    feat(repositories): implement get_stock_by_cell_code in warehouse_stock

--- backend/app/db/repositories/warehouse_stock.py
+++ backend/app/db/repositories/warehouse_stock.py
@@ -16,6 +16,24 @@ class WarehouseStockRepository(
         super().__init__(WarehouseStock, session)
 
 
+    async def get_stock_by_cell_code(
+        self,
+        cell_code: str
+    ) -> WarehouseStock | None:
+        
+        stmt = (
+            select(
+                WarehouseStock
+            )
+            .where(WarehouseStock.cell_code == cell_code)
+            .options(selectinload(WarehouseStock.product))
+        )
+
+        result = await self.session.execute(stmt)
+
+        return result.scalar_one_or_none()
+
+
     async def get_product_in_warehouse(
         self,
         product_id: int
