7dcd9ba833b148d6fbbd3e2b4b58a2cf1ebd8834
egogoboy <egor2005sudakov@gmail.com>
2026-05-30 20:38:35 +0300

    fix(schemas): change shop_stock schemas and add new

--- backend/app/schemas/shop_stock.py
+++ backend/app/schemas/shop_stock.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel, ConfigDict
 
 from app.schemas.product import ProductView
+from app.schemas.shop import ShopView
 
 
 class ShopStockBaseModel(BaseModel):
     model_config = ConfigDict(from_attributes=True)
+
 
 class ShopStockCreate(ShopStockBaseModel):
     shop_id: int
@@ -14,9 +16,28 @@ class ShopStockCreate(ShopStockBaseModel):
 
 
 class ShopStockView(ShopStockBaseModel):
+    shop: ShopView
     product: ProductView
     min_quantity: int
     quantity: int
+
+
+class ShopStockWithProductView(ShopStockBaseModel):
+    product: ProductView
+    min_quantity: int
+    quantity: int
+
+
+class ShopStockWithShopView(ShopStockBaseModel):
+    shop: ShopView
+    min_quantity: int
+    quantity: int
+
+
+class ProductInShopsView(ShopStockBaseModel):
+    product: ProductView
+    count: int
+    items: list[ShopStockWithShopView]
 
 
 class ShopStockUpdate(ShopStockBaseModel):
@@ -27,9 +48,21 @@ class ShopStockUpdate(ShopStockBaseModel):
 
 
 class ShopStockList(ShopStockBaseModel):
-    shop: ShopStockShopView
+    shop: ShopView
     count: int
-    items: list[ShopStockView]
+    items: list[ShopStockWithProductView]
+
+
+class UpdateShopStockQuantity(ShopStockBaseModel):
+    shop_id: int
+    product_id: int 
+    quantity: int
+
+
+class UpdateShopStockMinQuantity(ShopStockBaseModel):
+    shop_id: int
+    product_id: int 
+    min_quantity: int
 
 
 class ShopStockShopView(ShopStockBaseModel):
