8f3bfa67863b0ea69534b52495f4b53b826cc1de
egogoboy <egor2005sudakov@gmail.com>
2026-05-31 15:30:43 +0300

    feat(api): implement shop stock api in shop

--- backend/app/api/v1/shop.py
+++ backend/app/api/v1/shop.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 
-from app.api.dependencies import get_shop_service
+from app.api.dependencies import get_shop_service, get_shop_stock_service
 from app.schemas.shop import ShopCreate, ShopList, ShopUpdate, ShopView
+from app.schemas.shop_stock import (ShopStockCreate, ShopStockList,
+                                    ShopStockView, UpdateShopStockMinQuantity)
 from app.services.shop import ShopService
+from app.services.shop_stock import ShopStockService
 
 router = APIRouter(prefix='/shops')
 
@@ -16,7 +19,7 @@ async def get_all_shops(
     return await shop_service.get_all()
 
 
-@router.get("/{id}", response_model=ShopView)
+@router.get("/", response_model=ShopView)
 async def get_shop_by_id(
     id: int,
     shop_service: ShopService = Depends(get_shop_service)
@@ -34,7 +37,7 @@ async def update_shop(
     return await shop_service.update(schema)
 
 
-@router.delete("/{id}")
+@router.delete("/")
 async def delete_shop_by_id(
     id: int,
     shop_service: ShopService = Depends(get_shop_service)
@@ -52,3 +55,52 @@ async def create_shop(
 ):
 
     return await shop_service.create_shop(schema)
+
+
+@router.post('/stocks', response_model=ShopStockView)
+async def create_shop_stock(
+    schema: ShopStockCreate,
+    shop_stock_service: ShopStockService = Depends(get_shop_stock_service)
+):
+
+    return await shop_stock_service.create_stock(schema)
+
+
+@router.get('/stocks', response_model=ShopStockView)
+async def get_shop_stock(
+    shop_id: int,
+    product_id: int,
+    shop_stock_service: ShopStockService = Depends(get_shop_stock_service)
+):
+
+    return await shop_stock_service.get_full_stock_info(shop_id, product_id)
+
+
+@router.get('/{id}/stocks', response_model=ShopStockList)
+async def get_shop_stocks(
+    id: int,
+    shop_stock_service: ShopStockService = Depends(get_shop_stock_service)
+):
+
+    return await shop_stock_service.get_shop_stocks(id)
+
+
+@router.put('/stocks', response_model=ShopStockView)
+async def update_shop_stock_min_quantity(
+    schema: UpdateShopStockMinQuantity,
+    shop_stock_service: ShopStockService = Depends(get_shop_stock_service)
+):
+
+    return await shop_stock_service.update_stock_min_quantity(schema)
+
+
+@router.delete('/stocks')
+async def delete_shop_stock(
+    shop_id: int,
+    product_id: int,
+    shop_stock_service: ShopStockService = Depends(get_shop_stock_service)
+):
+
+    await shop_stock_service.delete_stock(shop_id, product_id)
+
+    return JSONResponse(content={'message' : 'Shop stock is succesfully deleted'}, status_code=200)
