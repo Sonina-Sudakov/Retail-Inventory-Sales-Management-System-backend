b6a6b6deff53ecea89deecb2c5696277b90de0b6
egogoboy <egor2005sudakov@gmail.com>
2026-05-31 13:14:33 +0300

    feat(api): implement sale and shop_stock dependencies

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -2,15 +2,15 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.dependencies import get_session
-from app.db.models.shop import Shop
-from app.db.repositories.product import ProductRepository
-from app.db.repositories.shop import ShopRepository
-from app.db.repositories.user import UserRepository
-from app.db.repositories.order import OrderRepository
-from app.services.product import ProductService
-from app.services.shop import ShopService
-from app.services.user import UserService
+from app.db.repositories import (OrderRepository, ProductRepository,
+                                 SaleRepository, ShopRepository,
+                                 ShopStockRepository, UserRepository)
 from app.services.order import OrderService
+from app.services.product import ProductService
+from app.services.sale import SaleService
+from app.services.shop import ShopService
+from app.services.shop_stock import ShopStockService
+from app.services.user import UserService
 
 
 async def get_user_repository(
@@ -70,3 +70,40 @@ async def get_order_service(
 ) -> OrderService:
 
     return OrderService(repository, shop_repository, product_repository, user_repository)
+
+
+async def get_sale_repository(
+    session: AsyncSession = Depends(get_session)
+) -> SaleRepository:
+    
+    return SaleRepository(session)
+
+
+async def get_shop_stock_repository(
+    session: AsyncSession = Depends(get_session)
+) -> ShopStockRepository:
+
+    return ShopStockRepository(session)
+
+
+async def get_shop_stock_service(
+    shop_repository: ShopRepository = Depends(get_shop_repository),
+    product_repository: ProductRepository = Depends(get_product_repository),
+    shop_stock_repository: ShopStockRepository = Depends(get_shop_stock_repository)
+) -> ShopStockService:
+
+    return ShopStockService(shop_repository, product_repository, shop_stock_repository)
+
+
+async def get_sale_service(
+    sale_repository: SaleRepository = Depends(get_sale_repository),
+    shop_repository: ShopRepository = Depends(get_shop_repository),
+    user_repository: UserRepository = Depends(get_user_repository),
+    product_repository: ProductRepository = Depends(get_product_repository),
+    shop_stock_service: ShopStockService = Depends(get_shop_stock_service)
+) -> SaleService:
+
+    return SaleService(
+        sale_repository, shop_repository, user_repository,
+        product_repository, shop_stock_service
+    )
