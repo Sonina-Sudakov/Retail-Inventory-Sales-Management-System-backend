c2a6dd4bb61733b7ea975392501cbbd5b9e2f3f8
egogoboy <egor2005sudakov@gmail.com>
2026-06-01 15:43:28 +0300

    feat(models): implement works_in_shop field in user model

--- backend/app/db/models/user.py
+++ backend/app/db/models/user.py
@@ -1,14 +1,23 @@
 from datetime import datetime
 
-from sqlalchemy import Enum, String, func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import CheckConstraint, Enum, ForeignKey, String, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship, validates
 
 from app.db.models.base import Base
+from app.db.models.shop import Shop
 from app.enums import UserRole
 
 
 class User(Base):
     __tablename__ = 'users'
+
+    __table_args__ = (
+        CheckConstraint(
+            f"(role = '{UserRole.SHOPKEEPER.value}' AND works_in_shop_id IS NOT NULL) OR "
+            f"(role = '{UserRole.STOREKEEPER.value}' AND works_in_shop_id IS NULL)",
+            name="check_shopkeeper_has_shop",
+        ),
+    )
 
     id: Mapped[int] = mapped_column(
         primary_key=True
@@ -35,6 +44,15 @@ class User(Base):
         nullable=False
     )
 
+    works_in_shop_id: Mapped[int | None] = mapped_column(
+        ForeignKey('shops.id'),
+        nullable=True
+    )
+
+    works_in_shop: Mapped['Shop | None'] = relationship(
+        lazy='raise'
+    )
+
     created_at: Mapped[datetime] = mapped_column(
         server_default=func.now(),
         nullable=False
@@ -45,3 +63,18 @@ class User(Base):
         onupdate=func.now(),
         nullable=False
     )
+
+
+    @validates("works_in_shop_id", "role")
+    def validate_user_role_and_shop(self, key, value):
+
+        role = value if key == "role" else self.role
+        shop_id = value if key == "works_in_shop_id" else self.works_in_shop_id
+
+        if role == UserRole.SHOPKEEPER and shop_id is None:
+            raise ValueError("ShopKeeper must have works_in_shop_id populated.")
+
+        if role == UserRole.STOREKEEPER and shop_id is not None:
+            raise ValueError("StoreKeeper cannot have a works_in_shop_id.")
+
+        return value
